66a1872b23aa32ad2da7ec4d74da4ca73d038253
PootisMaan <dborodin836@gmail.com>
2022-02-07 22:39:26 +0200

    improved pagination

--- result/views.py
+++ result/views.py
@@ -11,30 +11,50 @@ def search(request):
         if request.GET['keyword']:
 
             if 'page' in request.GET:
-                page = request.GET['page']
-                page = int(page)
+                current_page = request.GET['page']
+                current_page = int(current_page)
             else:
-                page = 0
+                current_page = 0
 
-            page_offset = page * 10
+            # If user sends request with negative page number
+            # I shouldn't do it, this bug can be fixed in JS
+            if current_page < 0:
+                current_page = 0
+            
+            # Compensate that loops starts from 0
+            current_page -= 1
+            
+            # Request vars
+            page_offset = current_page * 10
             keyword = request.GET['keyword']
+            
             response = requests.get(
                 f'https://kitsu.io/api/edge/anime?filter[text]={keyword}&page[offset]={page_offset}')
+            
             raw_data = json.loads(response.text)
-
-            results = raw_data['meta']['count']
+            
+            results = raw_data['meta']['count']  # Amount of the found results
             last_page = ceil(results / 10) - 1
-
-            current_last_page = page + 5
+            
+            # Last clickable page
+            current_last_page = current_page + 5
             if current_last_page > last_page:
                 current_last_page = last_page
-            pages_list = [x for x in range(1, page)]
-            pages_list.extend([x + 1 for x in range(page, current_last_page)])
+
+            #  Depends which page is current
+            if current_page > 5:
+                current_first_page = current_page - 5
+            else:
+                current_first_page = 1
+            
+            # Generates list of possible links to pages
+            pages_list = [x for x in range(current_first_page + 2 if current_first_page > 3 else current_first_page, current_page + 1)]
+            pages_list.extend([x + 1 for x in range(current_page, current_last_page)])
 
             paginator = {
                 'has_other_pages': True if results > 10 else False,
-                'current': page,
-                'no_of_pages': [x + 1 for x in range(page, current_last_page)],  # Pages that are available
+                'current': current_page + 1,
+                'pages_list': pages_list,
                 'last': last_page,
             }
 
