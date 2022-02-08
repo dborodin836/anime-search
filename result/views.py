f1bc1b1f1f5efaa4d23efd4e376fadfb0645181b
PootisMaan <dborodin836@gmail.com>
2022-02-08 19:25:37 +0200

    some changes

--- result/views.py
+++ result/views.py
@@ -69,15 +69,55 @@ def search(request):
     return redirect('home')
 
 
-def detail(request, anime_id):
+def detail(request, anime_id, current_page=1):
+    # If user sends request with negative page number
+    # I shouldn't do it, this bug can be fixed in JS
+    if current_page < 1:
+        current_page = 1
+
+    # Compensate that loops starts from 0
+    current_page -= 1
+
+    # Request vars
+    page_offset = current_page * 10
+
     response = requests.get(f'https://kitsu.io/api/edge/anime?filter[id]={anime_id}')
-    raw_data = json.loads(response.text)
-    episodes_response = requests.get(f'https://kitsu.io/api/edge/anime/{anime_id}/episodes')
+    # raw_data = json.loads(response.text)
+    episodes_response = requests.get(f'https://kitsu.io/api/edge/anime/{anime_id}/episodes?page[offset]={page_offset}')
     raw_ep_data = json.loads(episodes_response.text)
+
+    raw_data = json.loads(response.text)
+
+    results = raw_ep_data['meta']['count']  # Amount of the found results
+    last_page = ceil(results / 10) - 1
+
+    # Last clickable page
+    current_last_page = current_page + 5
+    if current_last_page > last_page:
+        current_last_page = last_page
+
+    #  Depends which page is current
+    if current_page > 5:
+        current_first_page = current_page - 5
+    else:
+        current_first_page = 1
+
+    # Generates list of possible links to pages
+    pages_list = [x for x in range(current_first_page + 2 if current_first_page > 3 else current_first_page,
+                                   current_page + 1)]
+    pages_list.extend([x + 1 for x in range(current_page, current_last_page)])
+
+    paginator = {
+        'has_other_pages': True if results > 10 else False,
+        'current': current_page + 1,
+        'pages_list': pages_list,
+        'last': last_page,
+    }
 
     data = {
         'data': raw_data['data'][0],
         'ep_data': raw_ep_data['data'],
+        'paginator': paginator,
     }
     return render(request, 'result/detail.html', data)
 
